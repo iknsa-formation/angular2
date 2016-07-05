d288b2b5957ccbeeb82ac2469dcf0e678b4d31df
Khalid Sookia <khalidsookia@gmail.com>
2016-07-06 01:51:00 +0200

    removed article bootstrap and added directive to reddit app instead

--- app.ts
+++ app.ts
@@ -1,35 +1,6 @@
 import { bootstrap } from 'angular2/platform/browser';
 import { Component } from 'angular2/core';
 
-@Component({
-selector: 'reddit',
-template: `
-    <form class="ui large form segment">
-        <h3 class="ui header">Add a Link</h3>
-        <div class="field">
-            <label for ="title">Title:</label>
-            <input name="title" #newtitle>
-        </div>
-        <div class="field">
-            <label for ="link">Link:</label>
-            <input name="link" #newlink>
-        </div>
-        <button (click)="addArticle(newtitle, newlink)" class="ui positive right floated button">
-            Submit link
-        </button>
-    </form>
-
-    <div class="ui grid posts">
-        <reddit-article></reddit-article>
-    </div>
-`
-})
-class RedditApp {
-    addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
-        console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    }
-}
-
 
 @Component({
     selector: 'reddit-article',
@@ -82,5 +53,35 @@ class ArticleComponent {
     }
 }
 
+@Component({
+selector: 'reddit',
+directives: [ArticleComponent],
+template: `
+    <form class="ui large form segment">
+        <h3 class="ui header">Add a Link</h3>
+        <div class="field">
+            <label for ="title">Title:</label>
+            <input name="title" #newtitle>
+        </div>
+        <div class="field">
+            <label for ="link">Link:</label>
+            <input name="link" #newlink>
+        </div>
+        <button (click)="addArticle(newtitle, newlink)" class="ui positive right floated button">
+            Submit link
+        </button>
+    </form>
+
+    <div class="ui grid posts">
+        <reddit-article></reddit-article>
+    </div>
+`
+})
+class RedditApp {
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+        console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    }
+}
+
 bootstrap(RedditApp);
-bootstrap(ArticleComponent);
+// bootstrap(ArticleComponent);
