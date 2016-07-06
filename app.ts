0a735c0fd364722d39bdb04f14cd5ba6e6dd6a19
Khalid Sookia <khalidsookia@gmail.com>
2016-07-06 02:10:06 +0200

    moved votes logic to article class and calling the methods in the component

--- app.ts
+++ app.ts
@@ -11,6 +11,14 @@ class Article {
         this.link = link;
         this.votes = votes || 0;
     }
+
+    voteUp(): void {
+        this.votes += 1;
+    }
+
+    voteDown(): void {
+        this.votes -= 1;
+    }
 }
 
 @Component({
@@ -52,12 +60,12 @@ class ArticleComponent {
     }
 
     voteUp(): boolean {
-        this.article.votes += 1;
+        this.article.voteUp();
         return false;
     }
 
     voteDown(): boolean {
-        this.article.votes -= 1;
+        this.article.voteDown();
         return false;
     }
 }
